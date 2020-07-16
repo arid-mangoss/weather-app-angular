af8852d68c4b18560ba43783b46614d7d92e8673
Sam Snarr <sssnarr1@gmail.com>
2020-07-16 15:25:48 -0400

    added search functionality through api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,63 @@
 import { Component, OnInit } from '@angular/core';
 import { WeatherHttpService } from './service/weather-http.service';
 
+import { LocaleData } from './models/Locale';
+import { debounceTime, map, distinct, switchMap } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  city: string;
   weatherIcon: string;
   outputData: string;
+  data: LocaleData[] = [];
+  locale: string;
+  foundLocale: LocaleData;
+
+  keySubject = new Subject<any>();
 
   constructor(private weatherService: WeatherHttpService) {}
 
-  ngOnInit() {}
-
-  getData() {
-    this.weatherService.getCityData(this.city).subscribe((data) => {
-      console.log(data);
-      this.outputData = `${data.name}, ${data.sys.country} -- ${JSON.stringify(
-        data.weather[0]
-      )}`;
-      this.weatherIcon = data.weather[0].icon;
-    },
-    (err) =>{
-      console.log(err);
-      this.city = undefined;
-      this.weatherIcon = undefined;
-      this.outputData = undefined
+  ngOnInit() {
+    this.keySubject.pipe(debounceTime(200), distinct()).subscribe((input) => {
+      this.getData();
     });
+  }
+
+  sendKeyPress(input: string) {
+    console.log(input);
+    this.keySubject.next(input);
+  }
+
+  // sets
+  getData() {
+    console.log('getdata() called... ');
+    this.weatherService.getData(this.locale).subscribe(
+      (data) => {
+        this.foundLocale = {
+          locale: data.name,
+          country: data.sys.country,
+          description: data.weather[0].description,
+          temp: data.main.temp,
+          icon: data.weather[0].icon,
+        };
+      },
+      (err) => {
+        this.foundLocale = undefined;
+        console.log(err);
+      }
+    );
+  }
+
+  onSubmit() {
+    this.weatherService.addLocale(this.foundLocale);
+    this.data = this.weatherService.data;
+  }
+
+  removeLocale(index: number) {
+    this.weatherService.removeLocale(index);
   }
 }
---
+++ src/app/models/Locale.ts
@@ -0,0 +1,7 @@
+export interface LocaleData {
+  locale: string;
+  country: string;
+  temp: number;
+  description: string;
+  icon: string;
+}
